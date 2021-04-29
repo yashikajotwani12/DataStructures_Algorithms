28dfe1cff9e260aca767031056d687f3ef294e21
Yashika <yashikajotwani12@gmail.com>
2021-04-29 16:25:23 +0530

    changes

---
+++ graphs/10_interview_questions.cpp
@@ -0,0 +1,11 @@
+// 1. Breadth First Search (BFS)
+// 2. Depth First Search (DFS)
+// 3. Shortest Path from source to all vertices **Dijkstra**
+// 4. Shortest Path from every vertex to every other vertex **Floyd Warshall**
+// 5. To detect cycle in a Graph **Union Find**
+// 6. Minimum Spanning tree **Prim**
+// 7. Minimum Spanning tree **Kruskal**
+// 8. Topological Sort
+// 9. Boggle (Find all possible words in a board of characters)
+// 10. Bridges in a Graph
+
--- graphs/topologicalsort.cpp
+++ graphs/topologicalsort.cpp
@@ -10,8 +10,6 @@ class graph
     graph(int v);
     void addedge(int u, int v);
     void topologicalsort();
-
-
 };
 graph::graph(int v)
 {
@@ -81,3 +79,11 @@ int main()
  
     return 0;
 }
+//Applications: 
+// Topological Sorting is mainly used for scheduling jobs from the given dependencies among jobs. In computer science, applications of this type arise in instruction scheduling, ordering of formula cell evaluation when recomputing formula values in spreadsheets, logic synthesis, determining the order of compilation tasks to perform in make files, data serialization, and resolving symbol dependencies in linkers 
+//Complexity Analysis: 
+
+// Time Complexity: O(V+E). 
+// The above algorithm is simply DFS with an extra stack. So time complexity is the same as DFS which is.
+// Auxiliary space: O(V). 
+// The extra space is needed for the stack.
